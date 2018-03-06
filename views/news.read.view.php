5bb2626a20c68312f6758ff1478dc557e1ca6251
Damian972 <hyoma972@gmail.com>
2018-03-06 14:56:26 -0400

    Fix line 9

--- views/news.read.view.php
+++ views/news.read.view.php
@@ -3,15 +3,15 @@
     # Header
     require(TEMPLATES.'header.template.php');
 ?>
-    <br>
-    <div class="uk-container">
-        <div class="">
-            <h2 id="title"><?=$resp['name']?></h2>
+    
+    <div class="uk-container"><br>
+        <div>
+            <h2 id="title"><?=$resp['title']?></h2>
             <hr class="uk-divider-icon">
             <div class="uk-text-meta" id="meta">
                 <span>Par <?=$resp['author']?> le <span class="uk-text-bold"><?=date_format(date_create($resp['created_at']), 'j M, Y')?></span>.</span>
             </div><br>
-            <div class="uk-width-4-5@m" id="content" style="border: 1px solid red;">
+            <div class="uk-width-4-5@m" id="content">
                 <?=$resp['content']?>
             </div>
         </div>
